969eef011e06991b8ef45b4ab01db58ab4f3c40a
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-10-31 18:50:13 +0300

    Added auth and admin middleware

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,13 @@
 require('express-async-errors');
 const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
+const auth = require('../middlewares/auth');
+const admin = require('../middlewares/admin');
 
 const router = Router();
 
-router.post('/', UserController.createUserAccount);
+router.post('/', auth, admin, UserController.createUserAccount);
+
+// Admin token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NzI1MzAwMDB9.bl-4moHzWXgnvRML4HhlY2ZyKN462G8TyMcbYBN3qpo
 
 module.exports = router;
