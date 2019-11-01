769b57ffe53819099995e4838ef53be6eb69fd56
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-01 10:55:32 +0300

    created userSignup validation file

---
+++ models/validates/userSignup.validate.js
@@ -0,0 +1,18 @@
+const Joi = require('@hapi/joi');
+
+const validateUser = (user) => {
+  const schema = Joi.object().keys({
+    firstName: Joi.string().min(3).max(50).required(),
+    lastName: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(4).max(50).required(),
+    gender: Joi.string().required(),
+    jobRole: Joi.string().min(3).max(50).required(),
+    department: Joi.string().min(3).max(50).required(),
+    address: Joi.string().min(3).max(50).required(),
+    isAdmin: Joi.boolean(),
+  });
+  return schema.validate(user);
+};
+
+module.exports.validateSignup = validateUser;
