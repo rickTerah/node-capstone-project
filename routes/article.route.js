a37423ba6a3b2203a02e7783e371c581faf0e944
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-04 21:11:22 +0300

    added a delete article route

--- routes/article.route.js
+++ routes/article.route.js
@@ -7,5 +7,6 @@ const auth = require('../middlewares/auth');
 
 router.post('/', auth, ArticleController.createSingleArticle);
 router.patch('/:articleId', auth, ArticleController.updateSingleArticle);
+router.delete('/:articleId', auth, ArticleController.deleteSingleArticle);
 
 module.exports = router;
