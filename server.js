2ed16b92c48ad856c9ecafbaf9e9dbb21d3fcde1
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-04 10:03:00 +0300

    added categories endpoints

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = require('./app');
 
 const users = require('./routes/user.route');
 const gifs = require('./routes/gif.route');
+const categories = require('./routes/category.route');
 
 
 app.use(bodyParser.json());
 app.use('/v1/auth', users);
 app.use('/v1/gifs', gifs);
+app.use('/v1/categories', categories);
 
 const port = process.env.PORT || 2500;
 app.listen(port, console.log(`Listening to port ${port}...`));
