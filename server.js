e315e2079076eea096b9a7747c3c0db81455c33a
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-04 12:23:59 +0300

    added article post route

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = require('./app');
 const users = require('./routes/user.route');
 const gifs = require('./routes/gif.route');
 const categories = require('./routes/category.route');
+const articles = require('./routes/article.route');
 
 
 app.use(bodyParser.json());
 app.use('/v1/auth', users);
 app.use('/v1/gifs', gifs);
 app.use('/v1/categories', categories);
+app.use('/v1/articles', articles);
 
 const port = process.env.PORT || 2500;
 app.listen(port, console.log(`Listening to port ${port}...`));
