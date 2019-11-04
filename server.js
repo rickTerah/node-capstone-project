f7afcec6cc755b5075eae61d3a277600cca47b7f
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-04 08:34:32 +0300

    added post gifs endpoint

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const bodyParser = require('body-parser');
 const app = require('./app');
 
 const users = require('./routes/user.route');
+const gifs = require('./routes/gif.route');
 
 
 app.use(bodyParser.json());
 app.use('/v1/auth', users);
+app.use('/v1/gifs', gifs);
 
 const port = process.env.PORT || 2500;
 app.listen(port, console.log(`Listening to port ${port}...`));
