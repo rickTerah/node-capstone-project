2e0462dd6f9ecda13bf5fa835762de8dcba3b39f
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-04 16:18:56 +0300

    added createdBy column.

--- models/db/gif.js
+++ models/db/gif.js
@@ -8,7 +8,8 @@ module.exports = () => {
                     title VARCHAR (50) NOT NULL,
                     imageUrl VARCHAR (1024) NOT NULL,
                     createdOn timestamp with time zone NOT NULL,
-                    publicId VARCHAR (1024) NOT NULL
+                    publicId VARCHAR (1024) NOT NULL,
+                    createdBy VARCHAR (50) NOT NULL
                 )`);
     } catch (error) {
       console.log(error);
