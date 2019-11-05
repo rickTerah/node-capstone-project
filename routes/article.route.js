26f186635b70a3a8681a83518736b685ab6756a4
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-05 12:34:08 +0300

    added a get specific article route

--- routes/article.route.js
+++ routes/article.route.js
@@ -8,6 +8,7 @@ const auth = require('../middlewares/auth');
 
 router.post('/', auth, ArticleController.createSingleArticle);
 router.get('/', auth, ArticleController.getAllArticles);
+router.get('/:articleId', auth, ArticleController.getSingleArticle);
 router.patch('/:articleId', auth, ArticleController.updateSingleArticle);
 router.delete('/:articleId', auth, ArticleController.deleteSingleArticle);
 router.post('/:articleId/comment', auth, ArticleCommentController.writeComment);
