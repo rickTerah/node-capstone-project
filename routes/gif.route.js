10c0c6b49271570105a490bad07b92d211d344fb
Patrick Mwangi <patrickmwangi554@gmail.com>
2019-11-11 16:26:01 +0300

    changed routes endpoint

--- routes/gif.route.js
+++ routes/gif.route.js
@@ -11,10 +11,10 @@ router.use(fileUpload({
 }));
 
 
-router.post('/', auth, GifController.postGif);
-router.get('/', auth, GifController.getAllgifs);
-router.get('/:gifId', auth, GifController.getSingleGif);
-router.delete('/:gifId', auth, GifController.deleteGif);
-router.post('/:gifId/comment', auth, GifCommentController.writeComment);
+router.post('/v1', auth, GifController.postGif);
+router.get('/v1', auth, GifController.getAllgifs);
+router.get('/v1/:gifId', auth, GifController.getSingleGif);
+router.delete('/v1/:gifId', auth, GifController.deleteGif);
+router.post('/v1/:gifId/comment', auth, GifCommentController.writeComment);
 
 module.exports = router;
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,12 +1,10 @@
 require('express-async-errors');
 const { Router } = require('express');
 const UserController = require('../controllers/user.controller');
-const auth = require('../middlewares/auth');
-const admin = require('../middlewares/admin');
 
 const router = Router();
 
-router.post('/create-user', auth, admin, UserController.createUserAccount);
-router.post('/signin', UserController.loginUser);
+router.post('/v1/create-user', UserController.createUserAccount);
+router.post('/v1/signin', UserController.loginUser);
 
 module.exports = router;
